644415c5a990892192a3ebbf4472692025a3dc0a
Gef <gefestun@gmail.com>
2018-10-19 11:54:48 +0300

    add assert_regexp_equal => core.py

--- ApiTest/tests/folders/test.py
+++ ApiTest/tests/folders/test.py
@@ -1,4 +1,6 @@
 import pytest
+# from unittest import TestCase as tc
+# from pytest
 
 from tools.helper import Helper
 from cfg.folders.config import folders_config as cfg
@@ -20,5 +22,8 @@ class TestFolders(CoreTests):
         ('second_test'),
     ])
     def test_folders(self, name):
-        resp = Helper.send_api_request(self.helper, 'folders')
-        print(resp)
+        # resp = Helper.send_api_request(self.helper, 'folders')
+        import re
+        mh = re.compile('we (ae|a) liv.ng in (.*)!')
+        self.assert_regexp_equal('we are living in america!', mh, 'Fuck')
+        # print(resp)
