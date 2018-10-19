f614f59085d90e292098562652bda201917228f5
Gef <gefestun@gmail.com>
2018-10-19 12:01:24 +0300

    add assert_regexp_equal => core.py

--- ApiTest/core.py
+++ ApiTest/core.py
@@ -1,3 +1,9 @@
+import re
 
 class CoreTests:
-    pass
+
+    def assert_regexp_equal(self, actual, expected, msg):
+        if re.match(expected, actual) is not None:
+            pass
+        else:
+            raise AssertionError('{0}. {1}\n not equal\n {2}'.format(msg, actual, expected.pattern))
